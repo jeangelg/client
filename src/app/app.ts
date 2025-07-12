b5f2dc64f83e57fafcc2644716481d1345b4ca89
jeangelg <bubulubu001@gmail.com>
2025-07-12 15:05:32 -0600

    Creacion inicial del proyecto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'client';
+export class App implements OnInit {
+  
+  private http = inject(HttpClient);
+
+  protected title = 'Perritos';
+
+  ngOnInit(): void {
+   this.http.get("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0").subscribe({
+    next: response => console.log(response),
+    error: error => console.log(error),
+    complete: () => console.log('Completed the http request')
+   });
+  }
+
 }
